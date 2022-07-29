c18d25565d9db5356238cdf512f7e5687c0c6881
Cassidy Barr <90427280+Jinx2498@users.noreply.github.com>
2022-07-29 18:43:46 -0400

    Ceiling Fan Finished Code

---
+++ CeilingFan.java
@@ -0,0 +1,90 @@
+public class CeilingFan {
+
+    private String fanSpeed;
+    private String fanRotation;
+    private int speed;
+    private boolean rotation;
+
+    // default settings of the ceiling fan;
+    public CeilingFan() {
+
+        this.fanSpeed = "The fan is off.";
+        this.fanRotation = "The fan is set to rotate clockwise.";
+        this.speed = 0;
+        this.rotation = true;
+
+    }
+
+    // retrieves the current speed of the fan and sets it to a string;
+    public String getSpeed() {
+
+        switch (this.speed) {
+            case 0:
+                this.fanSpeed = "The fan is off.";
+                break;
+            case 1:
+                this.fanSpeed = "The fan is on setting 1 (low).";
+                break;
+            case 2:
+                this.fanSpeed = "The fan is on setting 2 (medium).";
+                break;
+            case 3:
+                this.fanSpeed = "The fan is on setting 3 (high).";
+            default:
+                break;
+        }
+
+        return this.fanSpeed;
+
+    }
+
+    // retrieves the current rotation of the fan and sets it to a string;
+    public String getRotation() {
+
+        if (this.rotation) {
+            this.fanRotation = "The fan is set to rotate clockwise.";
+        } else {
+            this.fanRotation = "The fan is set to rotate counter-clockwise";
+        }
+
+        return this.fanRotation;
+
+    }
+
+    // turns on/increases/turns off the speed of the ceiling fan;
+    public int pullSpeedCord() {
+
+        this.speed++;
+        if(this.speed == 4){ 
+            this.speed = 0;
+        }
+
+        return this.speed;
+
+    }
+
+    // changes the direction of the rotation of the ceiling fan;
+    public boolean pullRotationCord() {
+
+        this.rotation = !this.rotation;
+
+        return this.rotation;
+        
+    }
+
+    // retrieves and prints the current rotation of the fan;
+    public void getFanState(CeilingFan obj) {
+
+        System.out.println(obj);
+
+    }
+    
+    @Override
+    public String toString() {
+
+        return "Current State of the ceiling fan: \n" +  getSpeed() + "\n" + getRotation() + "\n"; 
+
+    }
+
+    
+}
---
+++ CeilingFanTester.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class CeilingFanTester {
+
+    public static void main(String[] args) {
+
+        // Developer Tests
+        CeilingFan myFan = new CeilingFan();
+        myFan.getFanState(myFan);
+        myFan.pullSpeedCord();
+        myFan.pullRotationCord();
+        myFan.getFanState(myFan);
+        myFan.pullSpeedCord();
+        myFan.pullRotationCord();
+        myFan.getFanState(myFan);
+        myFan.pullSpeedCord();
+        myFan.pullRotationCord();
+        myFan.getFanState(myFan);
+        myFan.pullSpeedCord();
+        myFan.pullRotationCord();
+        myFan.getFanState(myFan);
+
+        // User Tests
+        CeilingFan testerFan = new CeilingFan();
+        int exit = 0, command;
+        Scanner input = new Scanner(System.in);
+
+        while(exit == 0) {
+            System.out.print("Please enter a command for the ceiling fan. \n(1 = Get current state of fan. 2 = Pull speed cord. 3 = Pull rotation cord. 4 = Exit Fan Tester.): ");
+
+            command = 0;
+
+            try {
+                command = input.nextInt();
+            } catch (Exception e) {
+                // Handle InputMismatchException
+                System.out.println("\nInvalid input! Please enter a command number 0 - 4.\n");
+                input.nextLine();
+                continue;
+            }
+
+            
+            System.out.print("\n");
+            if(command <= 0 || command >= 5) {
+
+                System.out.println("Invalid Command!\n");
+
+            } else {
+
+                if(command == 1) {
+
+                    testerFan.getFanState(testerFan);
+
+                } else if(command == 2) {
+
+                    testerFan.pullSpeedCord();
+
+                } else if(command == 3) {
+
+                    testerFan.pullRotationCord();
+                    
+                } else if(command == 4) {
+
+                    exit = 1;
+
+                }
+
+            }
+            
+        }
+
+        System.out.println("Thank you for using my fan!\n");
+
+        input.close();
+
+    }
+}
